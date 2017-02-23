81b36ee0c4d08ef9382e041413862a1669b1893a
daton <tonydangelo123@gmail.com>
2017-02-23 16:10:50 +0100

    added menuItemComponent

---
+++ client/app/common/menuItem/menuItem.component.js
@@ -0,0 +1,12 @@
+import template from './menuItem.html';
+import controller from './menuItem.controller';
+import './menuItem.scss';
+
+let menuItemComponent = {
+  restrict: 'E',
+  bindings: {},
+  template,
+  controller
+};
+
+export default menuItemComponent;
