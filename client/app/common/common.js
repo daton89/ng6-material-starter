af625f4c8fe43dd3d213202181ec0aef7e1b40ce
daton <tonydangelo123@gmail.com>
2017-02-23 16:26:03 +0100

    added menuItem in sidenav

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -3,12 +3,14 @@ import Navbar from './navbar/navbar';
 import SideNav from './sidenav/sidenav.module';
 import Hero from './hero/hero';
 import User from './user/user';
+import MenuItem from './menuItem/menuItem';
 
 let commonModule = angular.module('app.common', [
   Navbar,
   Hero,
   User,
-  SideNav
+  SideNav,
+  MenuItem
 ])
 
   .name;
