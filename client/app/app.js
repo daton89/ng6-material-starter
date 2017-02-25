733ddfb2e01d76613dfc2971d9e31d87ce1a4bb7
daton89 <tonydangelo123@gmail.com>
2017-02-25 21:00:00 +0100

    check font-awesome

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import ngMaterial from 'angular-material';
-import 'font-awesome/css/font-awesome.css';
+import 'font-awesome/css/font-awesome.css'; // check css-loader
 import 'material-design-icons';
 import 'angular-material/angular-material.css';
 import 'normalize.css';
@@ -13,31 +13,28 @@ import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
 
-
-
-
 angular.module('app', [
-  ngMaterial,
-  uiRouter,
-  Material,
-  Common,
-  Components
-])
-  .config(($locationProvider, $urlRouterProvider, $stateProvider) => {
-    "ngInject";
+        ngMaterial,
+        uiRouter,
+        Material,
+        Common,
+        Components
+    ])
+    .config(($locationProvider, $urlRouterProvider, $stateProvider) => {
+        "ngInject";
 
-    $stateProvider.state('app', {
-      url: '/:lang',
-      params: { lang: 'it' },
-      abstract: true,
-      component: 'app'
+        $stateProvider.state('app', {
+            url: '/:lang',
+            params: { lang: 'it' },
+            abstract: true,
+            component: 'app'
 
+        })
+
+        $urlRouterProvider.otherwise('/');
+        // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
+        // #how-to-configure-your-server-to-work-with-html5mode
+        $locationProvider.html5Mode(true).hashPrefix('!');
     })
 
-    $urlRouterProvider.otherwise('/');
-    // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
-    // #how-to-configure-your-server-to-work-with-html5mode
-    $locationProvider.html5Mode(true).hashPrefix('!');
-  })
-
-  .component('app', AppComponent);
+.component('app', AppComponent);
